95cbcb7d4bd1b273faec49f95b55502d190b3d9c
cm00033 <coralysmaldonado@Coralyss-MacBook-Pro.local>
2022-01-19 08:23:43 -0500

    task upload:

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,8 +1,73 @@
-install.packages("swirl")
+# pat: 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
+#install.packages("swirl")
 library(swirl)
-
-
-
-
-
-
+swirl()
+c()
+?c
+sqrt()
+my_div
+getwd()
+ls()
+x <- 9 
+list.files()
+dir()
+?list.files
+args()
+args(list.files)
+old.dir <- getwd()
+dir.create("testdir")
+setwd("testdir")
+file.create("mytest.R")
+file.exists("mytest.R")
+file.info("mytest.R")
+file.rename("mytest.R","mytest2.R")
+file.copy("mytest2.R", "mytest3.R")
+file.path("mytest3.R")
+file.path('folder1', 'folder2')
+?dir.create
+dir.create(file.path('testdir2', 'testdir3'), recursive = TRUE)
+setwd(old.dir)
+1:20
+pi:10
+15:1
+?`:`
+seq(1,20)
+seq(0, 10, by=0.5)
+my_seq <- seq(5, 10, length=30)
+length(my_seq)
+1:length(my_seq)
+seq(along.with = my_seq)
+seq_along(my_seq)
+rep(0, times = 40)
+rep(c(0, 1, 2), times = 10)
+rep(c(0, 1, 2), each = 10)
+x <- c(44, NA, 5, NA)
+x*3
+y <- rnorm(1000)
+z <- rep(NA, 1000)
+my_data <- sample(c(y,z),100)
+my_na <- is.na(my_data)
+my_na
+sum(my_na)
+my_data
+0/0 
+Inf - Inf 
+x[1:10]
+x[is.na(x)]
+y <- x[!is.na(x)]
+y[y > 0]
+x[x > 0]
+x[!is.na(x) & x > 0]
+x[c(3, 5, 7)]
+x[0]
+x[3000]
+x[c(-2, -10)]
+x[-c(2, 10)]
+vect <- c(foo= 11, bar= 2, norf= NA)
+vect
+names(vect)
+vect2 <- c(11, 2, NA)
+names(vect2) <- c("foo", "bar", "norf")
+identical(vect, vect2)
+vect["bar"]
+vect[c("foo", "bar")]
