400317c42db7cf6b0f934d7b63b5506f130e8f24
Coralys <97537322+cm00033@users.noreply.github.com>
2022-02-25 00:39:23 -0400

    Add files via upload

---
+++ Task_07/task07.r
@@ -0,0 +1,11 @@
+install.packages("rvertnet")
+library(rvertnet)
+"rvertnet-packages"
+?rvertnet
+?vertNet
+vertsearch(taxon="mammals", state="idaho" )
+traitsearch(taxon="mammals", has_sex= TRUE)
+Data <-vertsearch(taxon="mammals", state="idaho" )
+traitsearch(taxon="mammals", has_sex= TRUE)
+Data
+
